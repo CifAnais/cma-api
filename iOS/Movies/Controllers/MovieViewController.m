44b2a9247f0e5f1de65b1f4f4a095a8f89444558
Aymeric Gallissot <hello@aymericgallissot.fr>
2014-07-09 18:46:57 +0200

    iOS: movie (working)

--- iOS/Movies/Controllers/MovieViewController.m
+++ iOS/Movies/Controllers/MovieViewController.m
@@ -15,6 +15,8 @@
 
 @property (nonatomic, strong) Movie *movie;
 @property (nonatomic, strong) UIImageView *coverImageView;
+@property (nonatomic, strong) UIToolbar *postToolBar;
+@property (nonatomic, strong) UIToolbar *deleteToolBar;
 
 @end
 
@@ -56,6 +58,25 @@
         [self downloadImage:self.movie.coverURL];
     }
     
+    self.postToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0, self.view.height - 137.0, self.view.width, 44.0)];
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *postLike = [[UIBarButtonItem alloc] initWithTitle:@"Like" style:0 target:nil action:nil];
+    UIBarButtonItem *postDislike = [[UIBarButtonItem alloc] initWithTitle:@"Dislike" style:0 target:nil action:nil];
+    UIBarButtonItem *postWatched = [[UIBarButtonItem alloc] initWithTitle:@"Watched" style:0 target:nil action:nil];
+    UIBarButtonItem *postWatchlist = [[UIBarButtonItem alloc] initWithTitle:@"Watchlist" style:0 target:nil action:nil];
+
+    NSArray *postItems = @[flexibleSpace, postLike, postDislike, postWatched, postWatchlist, flexibleSpace];
+    [self.postToolBar setItems:postItems];
+    
+    self.deleteToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0, self.view.height - 93.0, self.view.width, 44.0)];
+    UIBarButtonItem *deleteLike = [[UIBarButtonItem alloc] initWithTitle:@"Delete" style:0 target:nil action:nil];
+    UIBarButtonItem *deleteDislike = [[UIBarButtonItem alloc] initWithTitle:@"Delete" style:0 target:nil action:nil];
+    UIBarButtonItem *deleteWatched = [[UIBarButtonItem alloc] initWithTitle:@"Delete" style:0 target:nil action:nil];
+    UIBarButtonItem *deleteWatchlist = [[UIBarButtonItem alloc] initWithTitle:@"Delete" style:0 target:nil action:nil];
+    NSArray *deleteItems = @[flexibleSpace, deleteLike, deleteDislike, deleteWatched, deleteWatchlist, flexibleSpace];
+    [self.deleteToolBar setItems:deleteItems];
+
+    
     [CFAPI shared].delegate = self;
 }
 
@@ -70,6 +91,25 @@
     [super viewWillAppear:animated];
     
     [CFAPI shared].delegate = self;
+    
+    if([CFUser shared].user){
+        [self.view addSubview:self.postToolBar];
+        [self.view addSubview:self.deleteToolBar];
+    }
+    else{
+        [self.postToolBar removeFromSuperview];
+        [self.deleteToolBar removeFromSuperview];
+    }
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    
+    if([CFUser shared].user){
+        [self.postToolBar removeFromSuperview];
+        [self.deleteToolBar removeFromSuperview];
+    }
 }
 
 #pragma mark - Download Images
